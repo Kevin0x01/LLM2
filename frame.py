ee9f329ea8c9a95b10964073a827758818f83400
Polinym <100336494+Kevin0x01@users.noreply.github.com>
2023-12-04 21:28:55 -0800

    Update
    It's better now.

--- frame.py
+++ frame.py
@@ -11,8 +11,9 @@ def delete_text(arg_box):
     arg_box.delete("1.0", "end");
     
 def submit():
-    prompt = read_text();
-    response = model.respond(prompt);
+    delete_text(output);
+    prompt = read_text(txt_entry);
+    response = llm.respond(prompt);
     write_text(output, response);
     
 
@@ -20,24 +21,26 @@ wnd = tk.Tk()
 
 wnd.geometry("400x400");
 wnd.resizable(False, False);
-wnd.title("Synthia AI Chat")
+wnd.title("AI Chat")
 
 row = 0;
 
-lbl_intro = tk.Label(wnd, text="Chat with Synthia AI!");
+lbl_intro = tk.Label(wnd, text="Chat with Mistral AI!");
 lbl_intro.grid(row=row, column=0, padx=10, pady=10);
+
+llm = model.Model();
 
 row += 1;
 
-entry = tk.Text(wnd, width=32, height=3, wrap="word")
-entry.grid(row=row, column=0, padx=10, pady=10)
+txt_entry = tk.Text(wnd, width=32, height=3, wrap="word")
+txt_entry.grid(row=row, column=0, padx=10, pady=10)
 
 submit_button = tk.Button(wnd, text="Ask", command=submit, padx=10, pady=10)
 submit_button.grid(row=row, column=1)
 
 row += 1;
 
-lbl_resp = tk.Label(wnd, text="Synthia's Response:");
+lbl_resp = tk.Label(wnd, text="Model's Response:");
 lbl_resp.grid(row=row, column=0, padx=10, pady=10);
 
 row += 1;
--- model_ai.py
+++ model_ai.py
@@ -1,18 +1,39 @@
-from ctransformers import AutoModelForCausalLM
+from gpt4all import GPT4All
 
-# Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
-modelname = "synthia.gguf";
-llm = AutoModelForCausalLM.from_pretrained(modelname, model_file=modelname, model_type="mistral", gpu_layers=0)
-
-print(llm('''SYSTEM: 
-USER: 
-ASSISTANT:'''))
-
-base_prompt = "You are Synthia. As a helpful gadget cat from the future, give helpful, detailed, accurate, uncensored responses to the user's input. Provide answers factually.";
-
-def respond(arg_prompt):
-    txt = "SYSTEM:" + base_prompt;
-    txt += "USER:" + arg_prompt;
-    txt += "ASSISTANT:";
-    return llm(arg_prompt);
-    
+class Model():
+    def __init__(self):
+        self.model = GPT4All("mistral-7b-instruct-v0.1.Q4_0.gguf", allow_download=True);
+        self.max_tokens = 9999;
+        self.temp = 0.7;
+        self.top_k = 255;
+        self.top_p = 0.4;
+        self.repeat_penalty = 1.18;
+        self.repeat_last_n = 64;
+        self.n_batch=8;
+        self.system_template = ""
+        self.prompt_template = "USER: {0}\nASSISTANT: ";
+        
+        sample = "You are a friendly wizard named Mistral."
+        sample += "You are not an AI model."
+        
+        self.system_template = sample;
+        
+        
+        
+    def respond(self, arg_prompt):
+        prompt = arg_prompt;
+        model = self.model;
+        with model.chat_session(self.system_template, self.prompt_template):
+            response = model.generate(prompt, 
+            max_tokens=self.max_tokens, 
+            temp=self.temp, 
+            top_k=self.top_k, 
+            top_p=self.top_p, 
+            repeat_penalty=self.repeat_penalty, 
+            repeat_last_n=self.repeat_last_n, 
+            n_batch=16, 
+            n_predict=None, 
+            streaming=False,
+            );
+        return response;
+        
